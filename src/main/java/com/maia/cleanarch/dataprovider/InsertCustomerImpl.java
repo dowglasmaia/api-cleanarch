312ac2e8c1eb0f39f19923279f2b58e520e615aa
Dowglas Maia <dowglasmaia@gmail.com>
2023-07-12 10:51:13 -0300

    impl Insert customer no dataprovider

---
+++ src/main/java/com/maia/cleanarch/dataprovider/InsertCustomerImpl.java
@@ -0,0 +1,30 @@
+package com.maia.cleanarch.dataprovider;
+
+import com.maia.cleanarch.core.dataprovider.InsertCustomer;
+import com.maia.cleanarch.core.domain.Customer;
+import com.maia.cleanarch.dataprovider.repository.CustomerRepository;
+import com.maia.cleanarch.dataprovider.repository.mapper.CustomerMapper;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Log4j2
+public class InsertCustomerImpl implements InsertCustomer {
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private CustomerMapper customerMapper;
+
+    @Override
+    public void insert(Customer customer) {
+        var customerEntity = customerMapper.toCustomerEntity(customer);
+        try {
+            customerRepository.save(customerEntity);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("Failed to save Customer. {}", customer);
+        }
+    }
+}
+
---
+++ src/main/java/com/maia/cleanarch/dataprovider/repository/mapper/CustomerMapper.java
@@ -0,0 +1,11 @@
+package com.maia.cleanarch.dataprovider.repository.mapper;
+
+import com.maia.cleanarch.core.domain.Customer;
+import com.maia.cleanarch.dataprovider.repository.entity.CustomerEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CustomerMapper {
+    CustomerEntity toCustomerEntity(Customer customer);
+
+}
